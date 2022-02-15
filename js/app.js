d8026a2efcce862b49bd7531d65d7ea395cdf72b
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-15 17:59:45 +0600

    new code added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 // this is for total price
-function getTotalPrice(memoryCost) {
+function getTotalPrice() {
+  const memoryCost = document.getElementById("memory-cost");
   const bestPrice = document.getElementById("best-price");
   const totalPrice = document.getElementById("total-price");
+  const storageCost = document.getElementById("storage-cost");
+  const deliveryCost = document.getElementById("delivery-cost");
+
   totalPrice.innerText =
-    parseFloat(memoryCost.innerText) + parseFloat(bestPrice.innerText);
+    parseFloat(memoryCost.innerText) +
+    parseFloat(bestPrice.innerText) +
+    parseFloat(storageCost.innerText) +
+    parseFloat(deliveryCost.innerText);
 }
 // this is common function for memory cost
 function totalMemoryCost(checkMemory) {
@@ -13,7 +20,7 @@ function totalMemoryCost(checkMemory) {
     getTotalPrice(memoryCost);
   } else {
     memoryCost.innerText = 400;
-    getTotalPrice(memoryCost);
+    getTotalPrice();
   }
 }
 // this code for 8GB memory
@@ -23,4 +30,22 @@ document.getElementById("eightGB").addEventListener("click", function () {
 // this code for 16GB memory
 document.getElementById("sixteenGB").addEventListener("click", function () {
   totalMemoryCost(false);
+});
+
+function totalStorageCost(price) {
+  const storageCostInput = document.getElementById("storage-cost");
+  storageCostInput.innerText = price;
+}
+
+document.getElementById("ssd1").addEventListener("click", function () {
+  totalStorageCost(500);
+  getTotalPrice();
+});
+document.getElementById("ssd2").addEventListener("click", function () {
+  totalStorageCost(700);
+  getTotalPrice();
+});
+document.getElementById("ssd3").addEventListener("click", function () {
+  totalStorageCost(900);
+  getTotalPrice();
 });
